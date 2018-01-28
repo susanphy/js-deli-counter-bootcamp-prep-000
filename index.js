33c24d4a292ee657838faa682330a6d3da198ae5
Susan <suzutoyuki@gmail.com>
2018-01-28 04:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ while (i < katzDeli.length){i++;}
   var line=[];
 
   function currentLine(katzDeli) {
-
-    for (let i = 0; i < katzDeli.length; i++) {
- line.push(` `+[i+1]+`. `  + katzDeli[i])
-  }
- return(`The line is currently:` + line);
- }
+   let i = 0;
+    while (i < katzDeli.length) {
+       line.push(` `+[i+1]+`. `  + katzDeli[i])
+       i++;
+     }
+    if (katzDeli.length === 0) {
+      return "The line is currently empty.";
+     } else
+    return(`The line is currently:` + line);
+   }
